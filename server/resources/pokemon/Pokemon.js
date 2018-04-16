267279dcb6e07391ba063013c9ca801dccdce16d
Hussein <hmtirawi@gmail.com>
2018-04-16 10:20:56 +0300

    edit properities names

--- server/resources/pokemon/Pokemon.js
+++ server/resources/pokemon/Pokemon.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 // Complete the pokemonSchema below.
 
 var pokemonSchema = new mongoose.Schema({
-	'number': {type:Number,unique:true},
-	'name': {type:String,unique:true},
-	'types': String,
-	'imageUrl': String
+	number: {type:Number,unique:true},
+	name: {type:String,unique:true},
+	types: String,
+	imageUrl: String
 });
 
 
