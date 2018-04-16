55ba8f3fe898b0dcc744afdfb7219c5608020b71
Hussein <hmtirawi@gmail.com>
2018-04-16 10:07:11 +0300

    connect mongoose to my local mongo

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/pokemon';
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
+
+// to connect mongoose to our local DB
+mongoose.connect(mongoUri);
+
 var db;
 
 
