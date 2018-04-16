705092209b7456aade24d77cd4e5f4d8ca300825
alaamigdady <alaamohammad283@gmail.com>
2018-04-16 12:46:26 +0300

    pokemon routers

--- server/resources/pokemon/pokemonRouter.js
+++ server/resources/pokemon/pokemonRouter.js
@@ -2,9 +2,17 @@ var pokemonRouter = require('express').Router();
 var pokemonController = require('./pokemonController');
 
 // Create route handlers for each of the six methods in pokemonController
-pokemonRouter.route('/')
+pokemonRouter.route('/').get(pokemonController.retrieve);
 
-pokemonRouter.route('/:number')
+pokemonRouter.route('/').post(pokemonController.createOne);
+
+
+pokemonRouter.route('/').delete(pokemonController.delete);
+
+pokemonRouter.route('/:number').get(pokemonController.retrieveOne);
  
 
+pokemonRouter.route('/:number').put(pokemonController.updateOne);
+
+pokemonRouter.route('/:number').delete(pokemonController.deleteOne);
 module.exports = pokemonRouter;
