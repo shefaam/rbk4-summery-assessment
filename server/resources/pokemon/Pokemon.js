3e1da628cd1cfc71be919f6a05a5c676c1648b87
MohammadAlRifai <mohammad94sar@gmail.com>
2018-04-16 10:52:04 +0300

    bulid a schema

--- server/resources/pokemon/Pokemon.js
+++ server/resources/pokemon/Pokemon.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 // Complete the pokemonSchema below.
 
-var pokemonSchema;
+var pokemonSchema = mongoose.Schema({
+  number: Number,
+  name: String,
+  types: [],
+  imageUr:String
+});
 
 
 // Register the pokemonSchema with Mongoose as the 'Pokemon' collection.
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var morgan = require('morgan');
 var db = require('./db');
 
 // Create the Express application:
-var app;
+var app = express();
 
 
 // Attach middleware:
@@ -13,8 +13,27 @@ var app;
 // Import the pokemonRouter and assign it to the correct route:
 
 
-app.get('/', function (req, res) {
-	
+app.get('/api/pokemon', function (req, res) {
+	res.send('hi');
 });
 
+app.post('/api/pokemon', function (req, res) {
+  res.json('POST request to the homepage')
+});
+
+app.delete('/api/pokemon', function (req, res) {
+  res.send()
+});
+
+app.get('/api/pokemon/:number', function (req, res) {
+  res.send()
+});
+
+app.put('/api/pokemon/:number', function (req, res) {
+  res.json('POST request to the homepage')
+});
+
+app.delete('/api/pokemon/:number', function (req, res) {
+  res.send()
+});
 module.exports = app;
