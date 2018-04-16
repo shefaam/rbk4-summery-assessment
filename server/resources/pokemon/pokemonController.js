bf4a13314dc9a0788eaa605025aeead7869432ac
NaderNasr <nadernasr7@gmail.com>
2018-04-16 12:24:07 +0300

    pokemon controller

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -1,39 +1,56 @@
 var Pokemon = require('./Pokemon');
 var app = express()
+var data = body.json()
 
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
-var data = body.json()
+//create one table
 	app.get('/insertAllPokemon.js', function(data, err){
 		if ( err ){
 			req.send(err)
-			console.log ( 'ERROR HAS BEEN DETECTED' )
+			console.log ( 'CREATE ONE ERROR HAS BEEN DETECTED' )
 		} else {
 			res.send(data)
-			console.log ( 'RESPONSE SUCCESSFUL' )
+			console.log ( 'CREATE ONE RESPONSE SUCCESSFUL' )
 
-		}
+		};
 
-	})
+	});
 
 };
 
 exports.retrieve = function (req, res) {
-//find()
+//find() and retrieve all pokemons
+ app.find('/insertAllPokemon', function(data,err){
+ 	if( err ){
+ 		console.log ( 'RETRIEVE ERROR HAS BEEN DETECTED')
+ 	} else {
+ 		console.log ( 'RETRIEVE HAS BEEN SUCCESSFUL' )
+ 	}
+ })
+
+
 };
 
 exports.retrieveOne = function (req, res) {
-//findOne()
+//findOne() and retrieve one pokemons
+ app.findOne('/insertAllPokemon', function(data,err){
+ 	if( err ){
+ 		console.log ( 'RETRIEVE_ONE ERROR HAS BEEN DETECTED')
+ 	} else {
+ 		console.log ( 'RETRIEVE_ONE HAS BEEN SUCCESSFUL' )
+ 	}
+ })
 };
 
 exports.updateOne = function (req, res) {
-
+//update one pokemon 
 };
 
 exports.delete = function (req, res) {
-
+//delete all specified pokemons
 };
 
 exports.deleteOne = function (req, res) {
-
+//delete one pokemon
 };
