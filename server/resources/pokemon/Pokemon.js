1becef36abcaeebf96882f1fc4953587db9943c0
alaamigdady <alaamohammad283@gmail.com>
2018-04-16 12:09:53 +0300

    some controllers done

--- server/resources/pokemon/Pokemon.js
+++ server/resources/pokemon/Pokemon.js
@@ -13,15 +13,43 @@ var pokemonSchema = mongoose.Schema({
 
 var Pokemon = mongoose.model('Pokemon',pokemonSchema);
 
-function save(data){
+function save(data,callback){
 	//console.log(data)
 	var pokemon = new Pokemon({number:data.number,name:data.name,types:data.types,imageUrl:data.imageUrl})
-	pokemon.save(function(err){
+	pokemon.save(function(err,pokemon){
 			if (err) console.log('error')
+			callback(pokemon)
 		})
+
+};
+
+
+function fetch(callback){
+	Pokemon.find(function(err,pokemon){
+		if (err) console.log('error')
+			callback(pokemon)
+	})
+};
+
+function fetchOne(num,callback){
+	Pokemon.findOne({'number':num},function(err,pokemon){
+		if (err) console.log('error')
+			callback(pokemon)
+	})
 
 }
 
+function removeOne(num,callback){
+	Pokemon.remove({'number':num},function(err,pokemon){
+		if (err) console.log('error')
+			callback(pokemon)
+	})
+
+
+}
 
 module.exports.Pokemon = Pokemon;
 module.exports.save = save;
+module.exports.fetch = fetch;
+module.exports.fetchOne = fetchOne;
+module.exports.removeOne = removeOne;
--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -2,18 +2,31 @@ var Pokemon = require('./Pokemon');
 
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
+	Pokemon.save(req.body,function(data){
+		res.json(data)
+	})
+
 
 };
 
 exports.retrieve = function (req, res) {
+	Pokemon.fetch(function(data){
+		res.json(data)
+	})
 
 };
 
 exports.retrieveOne = function (req, res) {
+	var num = req.params.number;
+	Pokemon.fetchOne(num,function(data){
+		res.json(data)
+	})
 
 };
 
 exports.updateOne = function (req, res) {
+	var num = req.params.number;
+
 
 };
 
@@ -22,5 +35,11 @@ exports.delete = function (req, res) {
 };
 
 exports.deleteOne = function (req, res) {
+	var num = req.params.number;
+	Pokemon.removeOne(num,function(data){
+		res.json(data)
+		
+	})
+
 
 };
