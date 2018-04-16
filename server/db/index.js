6959698b27f735e2cf8c80319b68d706ee5ee03c
ZoulfaALBawab <zoulfaalbawab90@gmail.com>
2018-04-16 10:52:12 +0300

    first cmmt

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/pokemon';
 
+
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db;
 
--- server/server.js
+++ server/server.js
@@ -4,17 +4,34 @@ var morgan = require('morgan');
 var db = require('./db');
 
 // Create the Express application:
-var app;
+var app = express();
+
 
 
 // Attach middleware:
 
+// app.all('/secret', function (req, res, next) {
+//   console.log('Accessing the secret section ...')
+//   next() // pass control to the next handler
+// })
 
 // Import the pokemonRouter and assign it to the correct route:
 
+/////////////////////////////////////////////////////////
+app.get('/api/pokemon', function (req, res) {
 
-app.get('/', function (req, res) {
-	
+
+	res.send('Hellooo')
 });
 
+
+
+app.post('/api/pokemon', function (req, res) {
+
+
+  res.send('POST request to the homepage')
+});
+//
 module.exports = app;
+
+//
