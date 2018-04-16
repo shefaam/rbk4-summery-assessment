09961d21bd75c34b35952243831b5c1d5c660e53
Atheer83 <atheer.for.tech@gmail.com>
2018-04-16 11:00:03 +0300

    some work in mongoose -_-

--- server/server.js
+++ server/server.js
@@ -1,20 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var db = require('./db');
+var db = require('./db/index.js');
 
 // Create the Express application:
-var app;
-
+var app = express();
 
 // Attach middleware:
+
 
 
 // Import the pokemonRouter and assign it to the correct route:
 
 
-app.get('/', function (req, res) {
+app.get('/api/pokemon', function (req, res) {
+	res.send("Respond with JSON of all Pokémon")
+});
+
+app.post('/api/pokemon', function (req, res) {
 	
 });
+
+app.delete('/api/pokemon', function (req, res) {
+	
+});
+
+app.get('/api/pokemon', function (req, res) {
+	
+});
+
+app.get('/api/pokemon', function (req, res) {
+	
+});
+
+app.get('/api/pokemon', function (req, res) {
+	
+});
+
+
 
 module.exports = app;
