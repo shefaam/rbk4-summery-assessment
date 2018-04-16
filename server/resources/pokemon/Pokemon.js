38584b173ef9cc0d429c103f8a2339effc75e24d
Hussein <hmtirawi@gmail.com>
2018-04-16 10:08:01 +0300

    create Pokemon model

--- server/resources/pokemon/Pokemon.js
+++ server/resources/pokemon/Pokemon.js
@@ -1,12 +1,17 @@
 var mongoose = require('mongoose');
 // Complete the pokemonSchema below.
 
-var pokemonSchema;
+var pokemonSchema = new mongoose.Schema({
+	'number': {type:Number,unique:true},
+	'name': {type:String,unique:true},
+	'types': String,
+	'imageUrl': String
+});
 
 
 // Register the pokemonSchema with Mongoose as the 'Pokemon' collection.
 
-var Pokemon;
+var Pokemon=mongoose.model('Pokemon',schema);
 
 
 module.exports = Pokemon;
