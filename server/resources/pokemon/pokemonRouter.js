0d2cea06b6437347cf234bd0460b6f01c052e01b
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-16 11:14:21 +0300

    finished router

--- server/resources/pokemon/pokemonRouter.js
+++ server/resources/pokemon/pokemonRouter.js
@@ -4,7 +4,12 @@ var pokemonController = require('./pokemonController');
 // Create route handlers for each of the six methods in pokemonController
 pokemonRouter.route('/')
 
-pokemonRouter.route('/:number')
- 
+pokemonRouter.route('/:1')
+pokemonRouter.route('/:2')
+pokemonRouter.route('/:3')
+pokemonRouter.route('/:4')
+pokemonRouter.route('/:5')
+pokemonRouter.route('/:6')
 
 module.exports = pokemonRouter;
+
