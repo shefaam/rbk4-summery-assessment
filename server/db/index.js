31777b7afd4dd85bd593810d620faa551baa1ecd
samersalmeh <ssalmeh@gmail.com>
2018-04-16 15:38:17 +0300

    Assessment

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/pokemon';
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
-var db;
 
+/*var db=connect(mongoUri);*/
+var db = mongoose.createConnection(mongoUri);
 
 module.exports = db;
