bd0098cababbf4d2563e418a5370d7b9a1686e38
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-16 14:08:42 +0300

    controller almost done

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -1,5 +1,6 @@
 var Pokemon = require('./Pokemon');
-var db = require('./db');
+//i had a problem requireing db 
+// var db = require('db');
 // Complete each of the following controller methods
 
 // this function shout save some information about a pokemon character
@@ -12,29 +13,30 @@ exports.retrieve = function (req, res) {
 	db.Pokemon.find({}, function (err, data){
 		if (err){ throw err}
 			else { 
-				res.send(data)
+				res.send(data);
 			}
 	});
 };
 // this function will retrieve a specific character
 exports.retrieveOne = function (req, res) {
-	db.Pokemon.findOne({name: req.name}, function (err, data){
+	db.Pokemon.findOne({name: req.body.name}, function (err, data){
 		if (err){ throw err}
 			else { 
-				res.send(data)
+				res.send(data);
 			}
 	});
 };
 // this function will update the data of the characte
-
+// not sure how to implement it 
 exports.updateOne = function (req, res) {
- 	db.Pokemon.updateOne()
+ 	db.Pokemon.updateOne(req.body.name, option, changes);
 };
 
+// this function will remove the schema from the data base 
 exports.delete = function (req, res) {
-
+	db.Pokemon.remove();
 };
-
+// this function will delete a specific item of the schema 
 exports.deleteOne = function (req, res) {
-
+	db.Pokemon.remove({name: req.body.name})
 };
