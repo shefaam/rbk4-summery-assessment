e55c6897eb6dc33db5ea1ada91d71f224557baee
Ammarlio <ammarhalbouni@gmail.com>
2018-04-16 13:26:02 +0300

    BE1

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // Complete each of the following controller methods
+
 exports.createOne = function (req, res) {
 	var object={
 		number: req.body.number,
@@ -14,29 +15,62 @@ exports.createOne = function (req, res) {
 	}
 	Pokemon.create(object,function(err,Objdata){
 		if(err){
-			res.send(err)
+			console.log(err)
 		}
-		res.send(Objdata)
+		res.json(Objdata)
 	})
 
 };
 
 exports.retrieve = function (req, res) {
-
+	createOne(req,res){
+		Pokemon.save(function (err, product) {
+  		if (err) {
+  			console.log(err)
+  		}
+  		res.json(product)
+		})
+	}
 };
 
 exports.retrieveOne = function (req, res) {
+		Pokemon.findByName(object.name, function (err, data) {
+		retrieve(req,res){
+  			if(err){
+				console.log(err);
+			}
+				res.json(data);
 
+		}
+		});
+
+	}
 };
 
 exports.updateOne = function (req, res) {
+	createOne(req,res){
+		Pokemon.findOneAndUpdate(object.name, {$set:{name:"lol"}},function(err, data){
+    if(err){
+        console.log("Something wrong when updating data!");
+    }
 
+    res.json(data);
+});
+	}
 };
 
 exports.delete = function (req, res) {
-
+	createOne(req,res){
+	Pokemon.remove(object.number, function (err) {
+		console.log(err)
+	});
+	}
 };
 
 exports.deleteOne = function (req, res) {
-
+	createOne(req,res){
+	Pokemon.deleteOne(object.name, function (err) {
+		console.log(err)
+	});
+	}
 };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var db = require('./db');
+var router = require('./pokemon/pokemonRouter.js');
+var controller = require('./pokemon/pokemonController.js');
 // Create the Express application:
 var app = express();
 var jsonParser = bodyParser.json() 
@@ -11,10 +13,13 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 // Import the pokemonRouter and assign it to the correct route:
-
-
-app.get('/', function (req, res) {
-	
+app.post('/api/pokemon', function (req, res) {
+	controller.createOne(req,res){
+		res.send(req.body)
+	}
+});
+app.get('/api/pokemon', function (req, res) {
+	res.send(req.body)
 });
 
 module.exports = app;
