194be65a48a2ab59b36d8e840658820b025825b8
AhmadAlYaaqba <ahmad.y91@hotmail.com>
2018-04-16 09:59:44 +0300

    start working with back-end & database

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/pokemon';
-
+mongoose.connect(mongoUri);
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
-var db;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection to database not working'));
+db.once('open', function(){
+	console.log('connected to database work');
+});
+
+
+
+
 
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -4,17 +4,44 @@ var morgan = require('morgan');
 var db = require('./db');
 
 // Create the Express application:
-var app;
+var app = express();
 
 
 // Attach middleware:
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Import the pokemonRouter and assign it to the correct route:
 
+app.route('/pokemon')
+	.get(function(req, res) {
+		res.json('test');
+	})
+	.post(function(req, res) {
+		res.json('test');
+	})
+	.put(function(req, res) {
+		res.json('test'); 
+	})
 
 app.get('/', function (req, res) {
 	
 });
+
+// app.get('/pokemon', function(req, res) {
+
+// });
+
+// app.post('/pokemon', function(req, res) {
+
+// });
+
+// app.delete('/pokemon', function(req, res) {
+
+// });
+
+// app.put('/pokemon', function(req, res) {
+
+// });
 
 module.exports = app;
