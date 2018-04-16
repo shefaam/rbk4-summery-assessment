8c7ff4b3c4cf7cd7aa3b544a4a8296498973dae7
BushraAlabsi <boushra-93@live.com>
2018-04-16 16:17:14 +0300

    git and post in the front end are working

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -4,7 +4,99 @@ import Starter from './components/starter.jsx';
 import $ from 'jquery';
 
 
-const App = (props) => <Starter num={ props.number } />;
+// const App = (props) => <Starter num={ props.number } />;
+class App extends React.Component {
+constructor(props){
+	super(props)
+	this.state ={
+		inputs: {
+			name:'',
+			types:'',
+			imageUrl:''
+	},
+	number: 152,
+	pokemon: []
+}
+}
+
+onchange(property, e){
+	// console.log(property)
+	var inputs = this.state.inputs
+	// console.log(inputs)
+	inputs[property] = e.target.value;
+	// console.log(inputs)
+
+this.setState({inputs:inputs })
+}
+
+insert(){
+	var number = this.state.number;
+	this.setState ({
+		number : number+1
+	});
+	$.ajax({
+		method: 'POST',
+		url: '/api/pokemon',
+		data: {number: this.state.number, name: this.state.inputs.name, types:this.state.inputs.types, imageUrl:this.state.inputs.imageUrl},
+		success: function (data) {
+			alert("added successfully!")
+			console.log("success",data)
+		}
+	})
+	
+}
+
+search(){
+	var that = this;
+$.ajax({
+		method: 'GET',
+		url: '/api/pokemon',
+		data: {number: this.state.number, name: this.state.inputs.name, types:this.state.inputs.types, imageUrl:this.state.inputs.imageUrl},
+		success: function (data) {
+			console.log("success",data)
+			that.setState({pokemon : data})
+		}
+	})
+}
+back(){
+	this.setState({pokemon : []})
+}
+
+render(){
+	if(!this.state.pokemon.length){
+	return (
+		<div>
+		<h3>Add a pokemon</h3>
+		<p>insert the name</p>
+		<input value= {this.state.inputs.name} onChange={this.onchange.bind(this,'name')} /><br/>
+		<p>insert his types</p>
+		<input value= {this.state.inputs.types} onChange={this.onchange.bind(this,'types')} /><br/>
+		<p>insert an image url</p>
+		<input value= {this.state.inputs.imageUrl} onChange={this.onchange.bind(this,'imageUrl')} /><br/>
+		<button onClick = {this.insert.bind(this)}>ADD!</button><br/><br/><br/>
+		<button onClick = {this.search.bind(this)}>Display All Pokemons</button>
+		</div>
+		)}
+	else {
+		return (<div><h3 onClick = {this.back.bind(this)}>Go back to the main page</h3>
+				{this.state.pokemon.map((monster) =>
+					<div><h4>{monster.name}</h4>
+					<p> types: {monster.types}</p>
+					<img src={monster.imageUrl} /></div>
+					)}
+
+			</div>
+			)
+	}
+}
+}
+
+
+
+
+
+
+
 
 ReactDOM.render(<App number={ 10 }/>, document.getElementById('app'));
 
--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -49,20 +49,21 @@ exports.updateOne = function (req, res) {
 //   });
 // });
 db.Pokemon.update({number: myNumber}, { $set: { name :req.body.name, imageUrl : req.body.imageUrl, types : types}}, function(err,data){
-	res.send(data)
+	db.Pokemon.find({number: myNumber},function(err,modified){res.send(modified)})
 });
 }
 exports.delete = function (req, res) {
+	exports.retrieve(req,res);
 db.Pokemon.remove({}, function (err,data) {
   if (err) return handleError(err);
-  res.send(data)
 })
 };
 
 exports.deleteOne = function (req, res) {
 	myNumber = JSON.parse(req.url.split('/')[1])
+	db.Pokemon.find({number: myNumber},function(err,data){res.send(data)})
 db.Pokemon.remove({number: myNumber }, function (err,data) {
   if (err) return handleError(err);
-  res.send(data)
+  
 })
 };
