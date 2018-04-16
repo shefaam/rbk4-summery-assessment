e0333a37c924b038702c419a08f38f56ef83a6b1
HamzaAlwan <ihamzaalwan@gmail.com>
2018-04-16 11:26:11 +0300

    some modifications

--- server/resources/pokemon/pokemonRouter.js
+++ server/resources/pokemon/pokemonRouter.js
@@ -2,14 +2,14 @@ var pokemonRouter = require('express').Router();
 var pokemonController = require('./pokemonController');
 
 // Create route handlers for each of the six methods in pokemonController
-pokemonRouter.route('/')
+pokemonRouter.route('/');
 
-pokemonRouter.route('/:1')
-pokemonRouter.route('/:2')
-pokemonRouter.route('/:3')
-pokemonRouter.route('/:4')
-pokemonRouter.route('/:5')
-pokemonRouter.route('/:6')
+pokemonRouter.route('/:1');
+pokemonRouter.route('/:2');
+pokemonRouter.route('/:3');
+pokemonRouter.route('/:4');
+pokemonRouter.route('/:5');
+pokemonRouter.route('/:6');
 
 module.exports = pokemonRouter;
 
--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var db = require('./db');
+var pokemonRouter = require('pokemonRouter');
 
 // Create the Express application:
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Attach middleware:
 
