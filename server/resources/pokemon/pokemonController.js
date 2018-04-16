9d772a4893c0cc33ae989e05e03edfaf177bc13e
hmtirawi <hmtirawi@gmail.com>
2018-04-16 16:37:49 +0300

    adding to controller

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -5,15 +5,22 @@ var db = require('./db');
 
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
-db.insert()
+	db.insert(req.body)
 };
 
 exports.retrieve = function (req, res) {
+	db.find({},function(err,data){
+		for(var i = 0 ; i < data.length;i++){
 
+			res.send(data[i])
+		}
+	})
 };
 
 exports.retrieveOne = function (req, res) {
-
+	db.find({},function(err,data){
+		res.send(data)		
+	})
 };
 
 exports.updateOne = function (req, res) {
