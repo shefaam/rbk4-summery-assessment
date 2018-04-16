3cf81bd2d9ba8e2b7559c4a179a08abc6c4e1192
NaderNasr <nadernasr7@gmail.com>
2018-04-16 12:50:58 +0300

    Fixing Server

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/pokemon';
-
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
-var db;
-
+var db = require ('../db/index.js');
+var app = express()
+mongoose.connect('mongodb://localhost/pokemon')
 
 module.exports = db;
