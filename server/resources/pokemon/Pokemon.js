456af5be8b144535afb3927b5541fa5cd2979cf0
AymanGhaith <ayman.ghaith88@gmail.com>
2018-04-16 11:27:25 +0300

    create Pokemon model and registering it with mongoose

--- server/resources/pokemon/Pokemon.js
+++ server/resources/pokemon/Pokemon.js
@@ -1,12 +1,23 @@
 var mongoose = require('mongoose');
 // Complete the pokemonSchema below.
 
-var pokemonSchema;
+// - [ ] Create a Pokémon model in `resources/pokemon/Pokemon.js` and register it with Mongoose as the `Pokemon` collection with the following properties:
+//   - [ ] `number`, a unique number
+//   - [ ] `name`, a unique string
+//   - [ ] `types`, an array of strings
+//   - [ ] `imageUrl`, a string
+
+ var pokemonSchema = mongoose.Schema({
+  number: {type:Number, unique: true},
+  name: {type:String, unique: true},
+  types: [String],
+  imageUrl: String
+});
 
 
 // Register the pokemonSchema with Mongoose as the 'Pokemon' collection.
 
-var Pokemon;
+var Pokemon = mongoose.model('Pokemon', pokemonSchema);
 
 
 module.exports = Pokemon;
