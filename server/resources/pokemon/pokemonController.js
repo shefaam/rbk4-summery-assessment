d35cb12181fb8c4a651a068eb1bd83aa8c40ce90
sebo1234567 <s.jaafreh@yahoo.com>
2018-04-16 11:15:39 +0300

    hey

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -2,14 +2,27 @@ var Pokemon = require('./Pokemon');
 
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
-
+  new  Pokemon({name:req.body.name,number:req.body.number,types:req.body.types})
+  // res.send()
 };
 
 exports.retrieve = function (req, res) {
+	Pokemon.find(req,function (err, Pokemon) {
+  if (err) return console.log('err');
+ res.render(req.body);
+ //console.log(req.body)
+})
 
 };
 
 exports.retrieveOne = function (req, res) {
+Pokemon.findOne(function (err, person) {
+  if (err) return console.log('err');
+ res.render(req.body)
+// console.log(req.body)
+
+});
+
 
 };
 
@@ -18,9 +31,12 @@ exports.updateOne = function (req, res) {
 };
 
 exports.delete = function (req, res) {
+Pokemon.find().remove()
+res.render("success")
 
 };
 
 exports.deleteOne = function (req, res) {
-
+	Pokemon.deleteOne(req.body)
+	res.render("success")
 };
