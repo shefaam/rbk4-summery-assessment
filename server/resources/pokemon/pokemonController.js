7f5762f81254c3f6743f114af1813b121233abc9
TalOmari <talomari592@gmail.com>
2018-04-16 07:54:26 -0400

    tal assessment

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -1,26 +1,62 @@
 var Pokemon = require('./Pokemon');
-
+var pokemon=require('./Pokemon');
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
+	var newPokemon= new Pokemon(req.body);
+	newPokemon.save(err =>{
+		if(err){
+			res.send(err);
+		}
+		res.send(newPokemon)
+	})
 
 };
 
 exports.retrieve = function (req, res) {
+Pokemon.find(err,Pokemons)=>{
+	if(err){
+		res.send(err)
+	}
+	res.send(Pokemons)
+}
+
 
 };
 
 exports.retrieveOne = function (req, res) {
-
+	Pokemon.findOne(Pokemon,{number:'number'},function(err,pokemon){
+		if(err){
+			res.send(err);
+		}
+		res.send(pokemon)
+		}
+	})
 };
 
 exports.updateOne = function (req, res) {
-
+	Pokemon.findByIdAndUpdate(req.params,req.body,{new:true}function(err,Pokemon){
+		if(err){
+			res.send(err);
+		}
+		res.send(Pokemon)
+	})
+		
 };
 
 exports.delete = function (req, res) {
+	Pokemon.remove(function(err,Pokemon){
+		res.remove()
+	})
 
 };
 
 exports.deleteOne = function (req, res) {
+	Pokemon.deleteOne({number:'number'},function(err,pokemon){
+		if(err){
+			res.send(err);
+		}
+		res.send(pokemon)
+		}
 
+	})
 };
--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var db = require('./db');
-
+import pokemonRouter from './resources/pokemon/pokemonRouter.js'
 // Create the Express application:
-var app;
+var app=express();
 
 
 // Attach middleware:
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
 
 
 // Import the pokemonRouter and assign it to the correct route:
 
 
 app.get('/', function (req, res) {
-	
+	res.send('POKEMONS')
 });
 
 module.exports = app;
