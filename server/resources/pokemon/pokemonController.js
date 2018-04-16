adbee98237036b9e20d1f7c366a439b72358e811
Elena-Livia <elena.livia@yahoo.com>
2018-04-16 15:29:11 +0300

    v2

--- server/resources/pokemon/pokemonController.js
+++ server/resources/pokemon/pokemonController.js
@@ -1,26 +1,31 @@
 var Pokemon = require('./Pokemon');
-
+var data = require('../data/pokemon.json');
 // Complete each of the following controller methods
 exports.createOne = function (req, res) {
-
+ // var newPokemon = new Pokemon({
+ //   number: data.number,
+ //   name: data.name,
+ //   types: data.types,
+ //   imageUrl: data.imageUrl
+ // }) 
 };
 
 exports.retrieve = function (req, res) {
-
+  // var query = Pokemon.findAll()
 };
 
 exports.retrieveOne = function (req, res) {
-
+  // var query = Pokemon.findOne(err,data)
 };
 
 exports.updateOne = function (req, res) {
-
+  //var query = Pokemon.findOneAndUpdate(err,data)
 };
 
 exports.delete = function (req, res) {
-
+  //var query = Pokemon.remove()
 };
 
 exports.deleteOne = function (req, res) {
-
+  //var query = Pokemon.findOneAndRemove(err,data)
 };
--- server/server.js
+++ server/server.js
@@ -2,19 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var db = require('./db');
-
+var Pokemon = require('./resources/pokemon/Pokemon.js');
 // Create the Express application:
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//app.use(express.static(__dirname, '../react-client'));
 // Attach middleware:
 
 
 // Import the pokemonRouter and assign it to the correct route:
-
+app.post('/api/pokemon', function(req, res) {
+	 res.send(Pokemon.createOne())
+})
 
 app.get('/', function (req, res) {
-	res.send("hello")
+	res.send()
 });
 
 module.exports = app;
